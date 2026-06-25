9e5292ba8ff5ed5b4c35b3dade681be9326b2e04
Rishikesh <rishikeshy503@gmail.com>
2026-06-25 21:33:30 +0530

    Fix CORS configuration

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ app.use(cors({
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
 }));
-app.options("(.*)", cors());
+
 app.use(express.json());
 app.use(cookieParser());
 
