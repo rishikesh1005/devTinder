8fcbd747267b895026c0f86eea3bc0359306870c
Rishikesh <rishikeshy503@gmail.com>
2026-06-25 21:25:59 +0530

    deployment changes

--- src/app.js
+++ src/app.js
@@ -18,7 +18,10 @@ require('dotenv').config()
 app.use(cors({
     origin: process.env.FRONTEND_URL || "http://localhost:5173",
     credentials: true,
-}))
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
+app.options("*", cors());
 app.use(express.json());
 app.use(cookieParser());
 
